b196d0af0a4e074eaba73cc1345bb2e9d4b540ea
IAFahim <iafahim.dev@gmail.com>
2026-09-10 15:53:56 +0600

    Migrate package consumer to signed seek

--- tests/Tl.PackageConsumer/Program.cs
+++ tests/Tl.PackageConsumer/Program.cs
@@ -1,10 +1,9 @@
 using Tl;
 
 var total = default(PackageTotal);
-var input = new PackageTimeline.Input();
-var output = new PackageTimeline.Output(ref total);
-if (!Timeline.TryStart(PackageTimeline.Id, out var playback)
-    || !Timeline.TryForward(PackageTimeline.Id, in playback, 1u, in input, ref output, out playback))
+var playback = PackageTimeline.Start(0u);
+var data = new PackageTimeline.Data(ref playback, ref total);
+if (!PackageTimeline.TrySeek(ref data, 1))
     return 1;
 Console.WriteLine(total.Value);
 return 0;
@@ -17,11 +16,8 @@ public readonly struct PackageTrack : ITrack<PackageClip>
     public void Blend(in PackageClip first, in PackageClip second, float factor, out PackageClip result)
         => result = factor < 0.5f ? first : second;
 
-    public static void Forward(in Frame<PackageTrack, PackageClip> frame, ref PackageTotal total)
-        => total = new(total.Value + frame.Clip.Value);
-
-    public static void Backward(in Frame<PackageTrack, PackageClip> frame, ref PackageTotal total)
-        => total = new(total.Value - frame.Clip.Value);
+    public static void Seek(in Frame<PackageTrack, PackageClip> frame, ref PackageTotal total)
+        => total = new(total.Value + frame.Direction * frame.Clip.Value);
 }
 
 public readonly partial struct PackageTimeline : ITimeline
