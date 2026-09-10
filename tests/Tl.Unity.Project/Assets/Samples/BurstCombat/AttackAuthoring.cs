9bd77ccf284f85cc155544da0e0a7fdf557cd046
IAFahim <iafahim.dev@gmail.com>
2026-09-10 21:10:46 +0600

    test: execute the external Unity sample

--- tests/Tl.Unity.Project/Assets/Samples/BurstCombat/AttackAuthoring.cs
+++ tests/Tl.Unity.Project/Assets/Samples/BurstCombat/AttackAuthoring.cs
@@ -8,13 +8,19 @@ namespace Tl.Samples.BurstCombat
         public float PoseX;
         public float PoseY;
         public float Health = 100;
+        public uint StartGameTick = 100;
+        public int Delta = 11;
 
         private sealed class AttackBaker : Baker<AttackAuthoring>
         {
             public override void Bake(AttackAuthoring authoring)
             {
                 var entity = GetEntity(TransformUsageFlags.None);
-                AddComponent(entity, new AttackPlayback());
+                AddComponent(entity, new AttackPlayback
+                {
+                    StartGameTick = authoring.StartGameTick,
+                    Delta = authoring.Delta
+                });
                 AddComponent(entity, new CurrentPose { Value = new FighterPose { X = authoring.PoseX, Y = authoring.PoseY } });
                 AddComponent(entity, new NextPose());
                 AddComponent(entity, new FighterCombat { Value = new CombatStats { Health = authoring.Health } });
